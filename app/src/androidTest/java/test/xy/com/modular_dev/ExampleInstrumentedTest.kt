be9277e7d578f903bccb426ecc376851d9208fa8
qianyu <qianyu@meicai.cn>
2018-06-12 18:01:31 +0800

    init project

---
+++ app/src/androidTest/java/test/xy/com/modular_dev/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package test.xy.com.modular_dev
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("test.xy.com.modular_dev", appContext.packageName)
+    }
+}
